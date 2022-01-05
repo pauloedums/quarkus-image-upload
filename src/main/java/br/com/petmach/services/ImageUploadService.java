e4da592f3eef810608b1c131792c733a70b82627
Paulo Marques <pauloedums@gmail.com>
2022-01-05 19:33:53 -0300

    Adicionando compressao

--- src/main/java/br/com/petmach/services/ImageUploadService.java
+++ src/main/java/br/com/petmach/services/ImageUploadService.java
@@ -1,25 +1,30 @@
 package br.com.petmach.services;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.zip.GZIPOutputStream;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import br.com.petmach.model.PetRequestBody;
 import br.com.petmach.model.PetRequestDTO;
 import br.com.petmach.model.PetResponseDTO;
 
-import java.io.IOException;
-import java.util.Base64;
-
 @ApplicationScoped
 public class ImageUploadService {
     
-    public PetResponseDTO createResponse(PetRequestDTO petRequestDTO){
+    public PetResponseDTO createResponse(PetRequestDTO petRequestDTO) throws IOException{
         
         PetResponseDTO petResponseDTO = new PetResponseDTO();
 
-        String encodedMime = Base64.getEncoder().encodeToString(petRequestDTO.getFile());
+        byte[] compressed = compress(petRequestDTO.getFile());
+
+        String encodedMime = Base64.getEncoder().encodeToString(compressed);
+        String imageFile = new String("data:image/"+ petRequestDTO.getFileExtension() +";base64," + encodedMime.substring(0, 51) + "..."+ encodedMime.substring(Math.max(0, encodedMime.length() - 3)));
 
         petResponseDTO.setId(petRequestDTO.getId());
-        petResponseDTO.setFile(encodedMime);
+        petResponseDTO.setFile(imageFile);
         petResponseDTO.setFileName(petRequestDTO.getFileName());
         petResponseDTO.setFileExtension(petRequestDTO.getFileExtension());
 
@@ -31,12 +36,26 @@ public class ImageUploadService {
         PetRequestDTO petRequestDTO = new PetRequestDTO();
 
         byte[] fileContent = petRequestBody.getFile().readAllBytes();
-        byte[] encodedAsBytes = java.util.Base64.getMimeEncoder().encode(fileContent);
+        byte[] encodedAsBytes = Base64.getMimeEncoder().encode(fileContent);
+
+        byte[] compressed = compress(encodedAsBytes);
                 
-        petRequestDTO.setFile(encodedAsBytes);
+        petRequestDTO.setFile(compressed);
         petRequestDTO.setFileName(petRequestBody.getFileName());
         petRequestDTO.setFileExtension(petRequestBody.getFileExtension());
 
         return petRequestDTO;
     }
+    
+    public static byte[] compress(final byte[] bytesImage) throws IOException {
+        if ((bytesImage == null) || (bytesImage.toString().length() == 0)) {
+          return null;
+        }
+        ByteArrayOutputStream obj = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(obj);
+        gzip.write(bytesImage);
+        gzip.flush();
+        gzip.close();
+        return obj.toByteArray();
+      }
 }
